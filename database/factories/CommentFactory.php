b11d0a9b3b0e5f3f295bc032a4d858302e6b6e63
madajabbar22 <madajabbar22@gmail.com>
2021-06-24 17:30:11 +0700

    update

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Comment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'article_id'=>$this->faker->randomNumber(1,20),
+            'comment'=>$this->faker->text(100),
+        ];
+    }
+}
